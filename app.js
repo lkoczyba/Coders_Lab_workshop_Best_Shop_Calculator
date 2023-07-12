a7c74170770386ff9538949ab20612dc742e9aaf
lkoczyba <lukasz.koczyba@gmail.com>
2023-07-12 23:46:01 +0200

    get elements

---
+++ app.js
@@ -0,0 +1,19 @@
+//form elements
+const form={
+    products: document.getElementById('products'),
+    orders: document.getElementById('orders'),
+    package: document.getElementById('package'),
+    accounting: document.getElementById('accounting'),
+    terminal: document.getElementById('terminal')
+}
+
+//summary elements
+const summary={
+    products: document.getElementById('products'),
+    orders: document.getElementById('orders'),
+    package: document.getElementById('package'),
+    accounting: document.getElementById('accounting'),
+    terminal: document.getElementById('terminal'),
+    totalPrice: document.getElementById('total-price')
+}
+
